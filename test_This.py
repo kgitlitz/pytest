63ce774d9f0be37a5583d4ed904f13d653cd1f0c
Kenneth Gitlitz <kgitlitz@ccsnh.edu>
2022-11-03 14:48:41 -0400

    env vars

--- test_This.py
+++ test_This.py
@@ -3,3 +3,5 @@ def test_easy():
 	print('hello')
 	assert 5 == 5
 
+
+
--- test_UpDown.py
+++ test_UpDown.py
@@ -10,13 +10,22 @@ test_bucket = "kzy334x6"
 test_file = "test.txt"
 object_name = "33hellox"
 
+@pytest.fixture(scope='function')
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+    os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+
 @mock_s3 #decorate for mocking
 def test_s3(): #called by pytest 
 
-	s3_create_bucket('localstack', test_bucket)
+	s3_create_bucket(aws_credentials, test_bucket)
 	
 	try:
-		response = s3_upload('localstack', test_file, test_bucket, object_name)
+		response = s3_upload(aws_credentials, test_file, test_bucket, object_name)
 		print(f'Upload Response: {response}')
 	except ClientError as e:
 		print("error: ", e)
