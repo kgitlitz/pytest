e99d458e07873912c5562793a3db4b8a7694fbf8
Kenneth Gitlitz <kgitlitz@ccsnh.edu>
2022-11-03 19:14:23 -0400

    reducedv2

--- test_UpDown.py
+++ test_UpDown.py
@@ -12,15 +12,6 @@ object_name = "33hellox"
 
 profile = "default"
 
-@pytest.fixture(scope='function')
-def aws_credentials():
-    """Mocked AWS Credentials for moto."""
-    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
-    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
-    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
-    os.environ['AWS_SESSION_TOKEN'] = 'testing'
-    os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
-
 @mock_s3 #decorate for mocking
 def test_s3(): #called by pytest 
 
